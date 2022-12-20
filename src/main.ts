cbc199d11b2ad25f7621d95a78e83d174a62be0e
Oleksandr <88oleksandr@gmail.com>
2022-12-20 09:21:37 +0200

    change cors origin

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function start() {
     const app = await NestFactory.create(AppModule);
     app.setGlobalPrefix('api');
-    app.enableCors();
+    app.enableCors({
+        allowedHeaders: ['content-type'],
+        origin: ['http://localhost:3000', 'https://next-store-liard-three.vercel.app/'],
+        credentials: true,
+    });
     await app.listen(process.env.PORT, () => {
         console.log(`Server started on ${process.env.PORT}`);
     });

01f5dd4eb9f8cb96b528c46b18289944636fd769
Oleksandr <88oleksandr@gmail.com>
2022-12-20 09:14:23 +0200

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function start() {
-    const app = await NestFactory.create(AppModule, { cors: true });
+    const app = await NestFactory.create(AppModule);
     app.setGlobalPrefix('api');
+    app.enableCors();
     await app.listen(process.env.PORT, () => {
         console.log(`Server started on ${process.env.PORT}`);
     });
